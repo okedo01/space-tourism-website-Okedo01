05ec7bd8e01734c690585f40bbf563cb27a08e99
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-03 20:31:50 +0300

    Styling the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
- import "../src/App.css"
+ import { Route, Routes } from "react-router-dom"
+import "../src/App.css"
 import Navbar from "./Components/Navbar"
+import Home from "./Pages/Home"
+import Crew from "./Pages/Crew"
+import Destination from "./Pages/Destination"
+import Technology from "./Pages/Technology"
 
 function App() {
 
   return (
     <div className="space-container">
-      <Navbar />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/destination" element={<Destination />} />
+            <Route path="/crew" element={<Crew />} />
+            <Route path="/technology" element={<Technology />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,8 +3,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return ( 
-        <div className="nav-links">
+        <div style={{
+            background: "red"
+        }}>
             <Link to="/">Home</Link>
+            <Link to="/destination">Destination</Link>
+            <Link to="/crew">Crew</Link>
+            <Link to="/technology">Technology</Link>
         </div>
      );
 }
