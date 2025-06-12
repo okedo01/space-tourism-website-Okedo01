5148b09556a3a5e90c6879be1173dc9336c68744
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-12 16:07:02 +0300

    CSS Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from '../src/Navbar'
+import Dashboard from './Dashboard'
 import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/navs" element={<Navbar />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   )
