3e8c84ff49e12112c66721177888398dbef2b694
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-18 18:46:07 +0300

    Crew component

--- src/Pages/Crew.tsx
+++ src/Pages/Crew.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Crew: React.FC = () => {
   return (
-    <div>Crew</div>
+    <div>
+      <h3>MEET YOUR CREW</h3>
+      <div>
+        <div style={{
+          display: "flex",
+          background: "red"
+        }}>
+          <h4>FLIGHT ENGINEER <span>ANOUSHEH ANSARI</span></h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga corporis labore ut dicta esse minus optio ratione voluptates eaque similique.</p>
+        </div>
+          <img src="" alt="engineer" />
+      </div>
+    </div>
   )
 }
 
