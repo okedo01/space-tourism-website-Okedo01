9d99fabc8442a12674ed8c6f92040f0ed9a81a4e
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-20 12:34:59 +0300

    Styling Home Button

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,8 +5,10 @@ const Home: React.FC = () => {
   return (
     <div className="home">
       <article className="space">
-        <h3>SO, YOU WANT TO TRAVEL TO <span style={{display: "block", fontSize: "90px"}}>SPACE</span></h3>
-        <p style={{fontSize: "14px", paddingRight: "4em"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi minima voluptatem expedita. Tenetur exercitationem, maiores modi eaque vero quae non repudiandae suscipit beatae! Nisi, dolor sequi vitae voluptate fuga officia.</p>
+        <h3>SO, YOU WANT TO TRAVEL TO <span style={{ display: "block", fontSize: "90px" }}>SPACE</span></h3>
+        <p style={{ fontSize: "14px", paddingRight: "4em" }}>Let’s face it; if you want to go to space, you might as well genuinely go to
+          outer space and not hover kind of on the edge of it. Well sit back, and relax
+          because we’ll give you a truly out of this world experience!</p>
       </article>
       <button className="explore">
         <Link to="destination">EXPLORE</Link>
