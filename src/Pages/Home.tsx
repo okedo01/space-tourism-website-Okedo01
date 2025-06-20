ff598706501f421a61b0a40b064a886045311f01
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-20 12:29:06 +0300

    Styling Home Button

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home: React.FC = () => {
   return (
     <div className="home">
-      <div className="space">
+      <article className="space">
         <h3>SO, YOU WANT TO TRAVEL TO <span style={{display: "block", fontSize: "90px"}}>SPACE</span></h3>
         <p style={{fontSize: "14px", paddingRight: "4em"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi minima voluptatem expedita. Tenetur exercitationem, maiores modi eaque vero quae non repudiandae suscipit beatae! Nisi, dolor sequi vitae voluptate fuga officia.</p>
-      </div>
-      <p className="explore"> EXPLORE </p>
+      </article>
+      <button className="explore">
+        <Link to="destination">EXPLORE</Link>
+      </button>
     </div>
   )
 }
