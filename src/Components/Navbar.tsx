26733f7109ebe4fd331e5cc0af8c924f48f84ec4
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-20 12:19:09 +0300

    Fixing the Home width

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -8,20 +8,20 @@ const Navbar: React.FC = () => {
     <div>
       <nav className='nav-bar'>
         <Link to="/" style={{marginLeft: "50px"}}>
-          <img src={Logo} alt="" />
+          <img src={Logo} alt="Logo" />
         </Link>
         <div className="nav-links">
           <Link to="/" style={{textDecoration: "none"}}>
-            Home
+           <h4><span>01</span>HOME</h4>
           </Link>
           <Link to="/crew" style={{textDecoration: "none"}}>
-            Crew
+            <h4><span>02</span>CREW</h4>
           </Link>
           <Link to="/dest" style={{textDecoration: "none"}}>
-            Destionation
+            <h4><span>03</span>DESTINATION</h4>
           </Link>
           <Link to="/tech" style={{textDecoration: "none"}}>
-            Technology
+            <h4><span>04</span>TECHNOLOGY</h4>
           </Link>
         </div>
       </nav>
