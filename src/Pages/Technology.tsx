f33a43cd026c5d02428d142207eccda07fc97c68
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-23 19:25:53 +0300

    Style the Technology Component and wait for mobile responsiveness

--- src/Pages/Technology.tsx
+++ src/Pages/Technology.tsx
@@ -21,12 +21,12 @@ const Technology: React.FC = () => {
 
   return (
     <div className="technology">
-      <h1 style={{paddingTop: "7em", fontSize: "20px"}}><span>03</span>SPACE LAUNCH 101</h1>
+      <h1 style={{marginLeft: "100px", paddingTop: "6em", fontSize: "20px", color: "white"}}><span style={{paddingRight: "15px"}}>03</span>SPACE LAUNCH 101</h1>
       <main className="wrapper">
         <section className="circle">
           { technology.map((value, index) => (
             <button onClick={() => setSelectedIndex(index)} style={{
-              backgroundColor: selectedIndex === index ? "white" : "gray",
+              backgroundColor: selectedIndex === index ? "white" : "transparent",
               color: selectedIndex === index ? "black" : "white"
               }}>
               {index + 1}
@@ -34,16 +34,17 @@ const Technology: React.FC = () => {
           ))} 
         </section>
         <section className="contents">
+          <p style={{fontSize: "11px", color: "gray"}}>THE TERMINOLOGY...</p>
           { selectedTechnology && (
-            <div>
-              <h2>{ selectedTechnology.name}</h2>
-              <p>{ selectedTechnology.description}</p>
+            <div style={{}}>
+              <h2 style={{textTransform: "uppercase", color: "white", fontSize: "46px"}}>{ selectedTechnology.name}</h2>
+              <p style={{marginRight: "187px", paddingTop: "20px", color: "gray", fontSize: "14px"}}>{ selectedTechnology.description}</p>
             </div>
           )}
         </section>
         <section className="images">
           { selectedTechnology && (
-            <img src={selectedTechnology.images.landscape} alt="technology-image" style={{width: "400px", height: "390px"}}/>
+            <img src={selectedTechnology.images.landscape} alt="technology-image" style={{width: "400px", height: "370px"}}/>
           )}
         </section>
       </main>
