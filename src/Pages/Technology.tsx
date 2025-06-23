d1d26bc9a776c84b292237ec1146f78c2f3765ca
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-23 17:16:54 +0300

    Display contents from Fetched data

--- src/Pages/Technology.tsx
+++ src/Pages/Technology.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import type { TechnologyData, Tourism } from '../Types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Technology: React.FC = () => {
     const [ technology, setTechnology ] = useState<TechnologyData[]>([]);
@@ -22,10 +24,27 @@ const Technology: React.FC = () => {
   return (
     <div className="technology">
       <h1 style={{paddingTop: "7em", fontSize: "20px"}}><span>03</span>SPACE LAUNCH 101</h1>
-      <main>
-        <section className="circle"></section>
-        <section className="contents"></section>
-        <section className="images"></section>
+      <main className="wrapper">
+        <section className="circle">
+          { technology.map((value, index) => (
+            <button onClick={() => setSelectedIndex(index)}>
+              <FontAwesomeIcon icon={faCircle} />
+            </button>
+          ))} 
+        </section>
+        <section className="contents">
+          { selectedTechnology && (
+            <div>
+              <h2>{ selectedTechnology.name}</h2>
+              <p>{ selectedTechnology.description}</p>
+            </div>
+          )}
+        </section>
+        <section className="images">
+          { selectedTechnology && (
+            <img src={selectedTechnology.images.landscape} alt="technology-image" />
+          )}
+        </section>
       </main>
     </div>
   )
