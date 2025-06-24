1352be4c31c6ca520b0e06bf43e75329b203de4f
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-24 10:33:19 +0300

    Home Component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,8 +6,8 @@ const Home: React.FC = () => {
     <div className="home">
       <main>
         <article className="space">
-          <h3>SO, YOU WANT TO TRAVEL TO <span style={{ display: "block", fontSize: "90px" }}>SPACE</span></h3>
-          <p style={{ fontSize: "14px", paddingRight: "4em" }}>Let’s face it; if you want to go to space, you might as well genuinely go to
+          <h3>SO, YOU WANT TO TRAVEL TO <span style={{ display: "block", fontSize: "100px" }}>SPACE</span></h3>
+          <p style={{ fontSize: "14px", paddingRight: "2em" }}>Let’s face it; if you want to go to space, you might as well genuinely go to
             outer space and not hover kind of on the edge of it. Well sit back, and relax
             because we’ll give you a truly out of this world experience!</p>
         </article>
