40d73444fba90f60e0c0532be7531ef935da533c
Okedo01 <eliahmwelangi01@gmail.com>
2025-06-25 10:25:06 +0300

    Crew Component

--- src/Pages/Crew.tsx
+++ src/Pages/Crew.tsx
@@ -6,6 +6,7 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons';
 const Crew: React.FC = () => {
   const [ crewValues, setCrewValues ] = useState<CrewData[]>([])
   const [ selectedIndex, setSelectedIndex ] = useState(0);
+  const [ isLoading, setIsLoading ] = useState(true);
 
   useEffect(() => {
     fetch("/data.json")
@@ -23,27 +24,27 @@ const Crew: React.FC = () => {
 
   return (
     <div className="crew">
-
-      <main className='crew-details'>
-        <img src={selectedCrew.images.png} alt="crew-img" style={{width: "28%"}}/>
-        <section className="btn">
-          { crewValues.map((value, index) => (
-          <button key={index} onClick={() => setSelectedIndex(index)} style={{color: selectedIndex === index? "white" : "gray"}}>
-            <FontAwesomeIcon icon={faCircle} style={{width: "70%"}}/>
-          </button>
-        ))}
-        </section>
-        <header><span>02</span>MEET YOUR CREW</header>
-        <section className="wrapper">
-          { selectedCrew && (
-          <main style={{display: "flex", justifyContent: "center", alignItems: "center", marginTop: "5em"}}>   
-            <article className="contents">
-              <span>{ selectedCrew.role }</span>
-              <h2>{ selectedCrew.name }</h2>
-              <p>{ selectedCrew.bio }</p>
-            </article>
-          </main>
-        )}
+      <header><span>02</span>MEET YOUR CREW</header>
+      
+      <main className="container">
+        <section className='crew-details'>
+          <img src={selectedCrew.images.png} alt="crew-img" style={{width: "28%"}}/>
+          <section className="btn">
+            { crewValues.map((value, index) => (
+            <button key={index} onClick={() => setSelectedIndex(index)} style={{color: selectedIndex === index? "white" : "gray"}}>
+              <FontAwesomeIcon icon={faCircle} style={{width: "70%"}}/>
+            </button>
+          ))}
+          </section>
+          <section className="wrapper">
+            { selectedCrew && ( 
+              <article className="contents">
+                <span>{ selectedCrew.role }</span>
+                <h2>{ selectedCrew.name }</h2>
+                <p>{ selectedCrew.bio }</p>
+              </article>
+          )}
+          </section>
         </section>
       </main>
     </div>
